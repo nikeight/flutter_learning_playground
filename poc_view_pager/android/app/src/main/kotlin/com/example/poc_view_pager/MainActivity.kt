a25e755bca785195d8ffd3c60810d90c4f2cdf2b
nikeight <jain.niket.052@gmail.com>
2023-01-23 18:23:07 +0530

    added : Infinite PagerView with PageView.Builder

---
+++ poc_view_pager/android/app/src/main/kotlin/com/example/poc_view_pager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poc_view_pager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ poc_view_pager/lib/MusicAlbum.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MusicAlbum {
+  final bool isFav;
+  final String albumName;
+  final MaterialColor color;
+
+  MusicAlbum(this.albumName, this.isFav, this.color);
+}
